83f2b7066f71cd43ac3f3641e07da5f4d4676b2d
dnardnar <philiphew@hotmail.sg>
2025-01-22 17:18:05 +0800

    level-3

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class UI {
-    private ArrayList<String> tasks = new ArrayList<>();
+    private ArrayList<Task> tasks = new ArrayList<>();
     public void printLine() {
         System.out.println("____________________________________________________________");
     }
@@ -27,13 +27,20 @@ public class UI {
                 break;
             } else if(command.equals("list")) {
                 listTasks();
+            } else if(command.startsWith("mark ")){
+                int index = Integer.parseInt(command.split(" ")[1]);
+                markTask(index);
+            } else if(command.startsWith("unmark ")) {
+                int index = Integer.parseInt(command.split(" ")[1]);
+                unmarkTask(index);
             } else {
                 addTask(command);
             }
         }
         scanner.close();
     }
-    public void addTask(String task) {
+    public void addTask(String description) {
+        Task task = new Task(description);
         tasks.add(task);
         printLine();
         System.out.println(" More??? Added: " + task);
@@ -45,6 +52,32 @@ public class UI {
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + ". " + tasks.get(i));
         }
+        printLine();
+    }
+    public void markTask(int index) {
+        if (index < 1 || index > tasks.size()) {
+            printLine();
+            System.out.println(" XXX try again!!");
+            printLine();
+            return;
+        }
+        tasks.get(index - 1).markDone();
+        printLine();
+        System.out.println(" That's crazyy!! Marking this task as done:");
+        System.out.println("   " + tasks.get(index - 1));
+        printLine();
+    }
+    public void unmarkTask(int index) {
+        if (index < 1 || index > tasks.size()) {
+            printLine();
+            System.out.println(" XXX try again!!");
+            printLine();
+            return;
+        }
+        tasks.get(index - 1).markNotDone();
+        printLine();
+        System.out.println(" What have you done!! This task is undone:");
+        System.out.println("   " + tasks.get(index - 1));
         printLine();
     }
 }
