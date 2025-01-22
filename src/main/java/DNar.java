ea1a3589163998977c05cb9ae740e3fac3f32a35
dnardnar <philiphew@hotmail.sg>
2025-01-22 17:11:22 +0800

    level-1

--- src/main/java/DNar.java
+++ src/main/java/DNar.java
@@ -1,7 +1,6 @@
 public class DNar {
     public static void main(String[] args) {
         UI ui = new UI();
-        ui.greet();
-        ui.exit();
+        ui.runbot();
     }
 }
--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class UI {
     public void printLine() {
         System.out.println("____________________________________________________________");
@@ -12,5 +13,21 @@ public class UI {
         printLine();
         System.out.println(" Bye. Don't come back!");
         printLine();
+    }
+    public void runbot() {
+        greet();
+        Scanner scanner = new Scanner(System.in);
+        String command;
+        while (true) {
+            command = scanner.nextLine();
+            if (command.equals("bye")) {
+                exit();
+                break;
+            }
+            printLine();
+            System.out.println(" Echo---ing: " + command);
+            printLine();
+        }
+        scanner.close();
     }
 }
