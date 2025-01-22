23f9ea85e14574dd870fae427dee5d3bd4c45d33
dnardnar <philiphew@hotmail.sg>
2025-01-22 17:07:21 +0800

    level-1

---
+++ src/main/java/DNar.java
@@ -0,0 +1,7 @@
+public class DNar {
+    public static void main(String[] args) {
+        UI ui = new UI();
+        ui.greet();
+        ui.exit();
+    }
+}
---
+++ src/main/java/UI.java
@@ -0,0 +1,16 @@
+public class UI {
+    public void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+    public void greet() {
+        printLine();
+        System.out.println(" Yo! I'm DNar");
+        System.out.println(" Can you?");
+        printLine();
+    }
+    public void exit() {
+        printLine();
+        System.out.println(" Bye. Don't come back!");
+        printLine();
+    }
+}
